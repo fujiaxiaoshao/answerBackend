956c73bc252888dfd9d6ec343aab6295bf907694
fuhao <fujiaxiaoshao@qq.com>
2016-11-25 12:54:39 +0800

    对第一个页面进行了完善，详情页前段进行了编写

--- answerBackend/src/main/java/com/wulijiexi/answer/controller/BackendController.java
+++ answerBackend/src/main/java/com/wulijiexi/answer/controller/BackendController.java
@@ -1,8 +1,17 @@
 package com.wulijiexi.answer.controller;
 
+import com.wulijiexi.answer.dao.ArticleDao;
+import com.wulijiexi.answer.dao.ArticleTypeDao;
+import com.wulijiexi.answer.model.Article;
+import com.wulijiexi.answer.model.ArticleType;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.ResponseBody;
+
+import java.util.ArrayList;
 
 /**
  * Created by fujiaxiaoshao on 2016/11/12.
@@ -10,12 +19,34 @@ import org.springframework.web.bind.annotation.RequestMethod;
 @Controller
 @RequestMapping("/backend")
 public class BackendController {
+    @Autowired
+    private ArticleDao articleDao;
+    @Autowired
+    private ArticleTypeDao articleTypeDao;
 
+    //后台首页
     @RequestMapping(value="/articles",method= RequestMethod.GET)
     public  String articles(){
-
         return "backend/articles.jsp";
-
     }
+
+    @RequestMapping(value="/articleContent",method= RequestMethod.GET)
+    public  String articleContent(){
+        return "backend/articleContent.jsp";
+    }
+    //加载图文类型
+    @RequestMapping(value="/loadarticleTypes",method= RequestMethod.GET)
+    public @ResponseBody ArrayList<ArticleType> loadarticleTypes(){
+        return articleTypeDao.getArticleTypes();
+    }
+
+
+    //分页加载图文
+    @RequestMapping(value="/loadarticles",method = RequestMethod.GET)
+    public @ResponseBody ArrayList<Article> loadarticles(@RequestParam("page") int page, @RequestParam("articleType") String  articleType) {
+        return articleDao.getArticles(page,articleType);
+    }
+
+
 
 }
--- answerBackend/src/main/webapp/WEB-INF/backend/js/articles.js
+++ answerBackend/src/main/webapp/WEB-INF/backend/js/articles.js
@@ -1,8 +1,59 @@
 window.onload=function(){
-
-    
+    loadarticleTypes();
+    loadarticles(1,"0")
 }
 
-function loadarticles() {
-    
+function loadarticles(page,articleType) {
+    var innerHTML="";
+    var string="loadarticles?page="+page+"&articleType="+articleType;
+    var request = new XMLHttpRequest();
+    string=encodeURI(string);
+    request.open("GET",string);
+    request.send();
+    request.onreadystatechange = function() {
+        if (request.readyState===4) {
+            if (request.status===200) {
+                var packJson=  JSON.parse(request.responseText);
+                for(var index = 0; index < packJson.length; index++){
+                    innerHTML+="<tr><td>"+packJson[index].articleId+"</td><td>"+packJson[index].title+"</td><td>"+packJson[index].articleType+"</td><td>修改 删除</td></tr>";
+                }
+                try{
+                    document.getElementById("articleList").innerHTML=innerHTML;
+                } catch(e) {
+                    setTBodyInnerHTML(document.getElementById("articleList"),innerHTML);
+                }
+            }
+        }
+    }
+}
+
+
+
+function loadarticleTypes() {
+    var innerHTML="";
+    var string="loadarticleTypes";
+    var request = new XMLHttpRequest();
+    string=encodeURI(string);
+    request.open("GET",string);
+    request.send();
+    request.onreadystatechange = function() {
+        if (request.readyState===4) {
+            if (request.status===200) {
+                var packJson=  JSON.parse(request.responseText);
+                for(var index = 0; index < packJson.length; index++){
+                    innerHTML+="<option value=\""+packJson[index].articleTypeId+"\">"+packJson[index].articleTypeContent+"</option>";
+                }
+                try{
+                    document.getElementById("articleType").innerHTML=innerHTML;
+                } catch(e) {
+                    setTBodyInnerHTML(document.getElementById("articleType"),innerHTML);
+                }
+            }
+        }
+    }
+}
+function setTBodyInnerHTML(tbody, html) {
+    var div = document.createElement('div');
+    div.innerHTML = "<table><tbody id='tableBody'>" + html + "</tbody></table>";
+    tbody.parentNode.replaceChild(div.firstChild.firstChild, tbody)
 }
